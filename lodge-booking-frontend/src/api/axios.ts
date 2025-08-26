9894a4e0e7ec4d94f4827ded200d9447abce39cb
venunathm30 <venunathm30@gmail.com>
2025-08-26 19:37:00 +0530

    new changes

--- lodge-booking-frontend/src/api/axios.ts
+++ lodge-booking-frontend/src/api/axios.ts
@@ -1,3 +1,4 @@
+"use client"
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
---
+++ lodge-booking-frontend/src/types/heroicons.d.ts
@@ -0,0 +1,4 @@
+declare module '@heroicons/react/24/solid';
+declare module '@heroicons/react/24/outline';
+declare module '@heroicons/react/20/solid';
+declare module '@heroicons/react/20/outline';
