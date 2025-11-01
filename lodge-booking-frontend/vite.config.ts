849e4427963a426822ceff652d15303dbb4690a2
venunath-m <venunathm30@gmail.com>
2025-11-01 13:52:30 +0530

    new changes

---
+++ lodge-booking-frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'] // 🔹 important
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 3
+      },
+      mangle: true,            // rename variables & functions
+      format: {
+        comments: false        // remove comments
+      }
+    }
+  }
+});
